53e2f59e53e2c1e794bd49403f9d204b3cb0f442
Adarsh-g-s <adarsh431593@gmail.com>
2017-04-09 10:39:05 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+/**
+ * TODO description
+ */
+public   class  Main {
+	
+	 private void  print__wrappee__Hello  (){
+		System.out.println("Hello");
+	}
+
+	
+	 private void  print__wrappee__Beautiful  (){
+		print__wrappee__Hello();
+		System.out.println("Beautiful");
+	}
+
+	
+	public void print(){
+		print__wrappee__Beautiful();
+		System.out.println("World");
+	}
+
+	
+	
+	public static void main(String[] args){
+		new Main().print();
+	}
+
+
+}
